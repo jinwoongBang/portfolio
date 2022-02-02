b458cc1ef1122fcee8a69a79efd31ca6ce502082
jinwoong-bang <jwbang@malgn.com>
2022-02-02 18:41:00 +0900

    feature : 헤더와 풋터 고정

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import Image from 'next/image';
 
 import type { LayoutProps } from 'src/components/layout/type';
 
+import Logo from 'public/mix_crop.png';
 import styles from 'src/components/layout/styles.module.css';
 
 function Layout({ children }: LayoutProps) {
   return (
     <div>
-      <header>
-        <nav>
-          <a>About</a>
-          <a>Skills</a>
-          <a>Work</a>
-          <a>Trouble Shooting</a>
+      <header className={styles.header}>
+        <div className={styles['logo-container']}>
+          <Image src={Logo} alt="Empty Logo" layout="fill" />
+        </div>
+        <nav className={styles.nav}>
+          <a href="./">About</a>
+          <a href="./">Skills</a>
+          <a href="./">Work</a>
+          <a href="./">Trouble Shooting</a>
         </nav>
       </header>
       <main className={styles.main}>{children}</main>
-      <footer>
+      <footer className={styles.footer}>
         <nav>
           <a href="">Instagram</a>
           <a href="">GitHub</a>
