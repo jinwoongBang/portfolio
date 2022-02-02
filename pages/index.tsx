2a4fbe1797f15834e774b2e73b4797df5c551c1c
jinwoong-bang <jwbang@malgn.com>
2022-02-02 19:09:22 +0900

    feature : 로고 및 내 사진 이미지 삽입

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,43 @@
 import type { NextPage } from 'next';
+import Image from 'next/image';
+
+import Logo from 'public/mix_crop.png';
+import Me from 'public/me.png';
+
+import styles from 'styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
     <div className="container mx-auto">
-      <h1>Main Page</h1>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="col-span-1 border border-white">
+          <div className={styles['image-container']}>
+            <div className={styles['image-container__logo']}>
+              <Image src={Logo} alt="Empty Image" layout="fill" />
+            </div>
+            <div className={styles['image-container__me']}>
+              <Image src={Me} alt="Empty Image" layout="fill" />
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 border border-white text-xl">
+          <h1>Hi,</h1>
+          <h1 className="">I&apos;m Jinwoong-Bang</h1>
+          <h1 className="text-white">Web Developer</h1>
+        </div>
+        <div className="col-span-1 border border-white">
+          <ul>
+            <li>&lt;Front-End-Developer&gt;</li>
+            <ul>
+              <li>&lt;Back-End-Developer /&gt;</li>
+            </ul>
+            <li>&lt;Front-End-Developer /&gt;</li>
+          </ul>
+        </div>
+        <div className="col-span-1 border border-white">
+          <button>About Me</button>
+        </div>
+      </div>
     </div>
   );
 };
