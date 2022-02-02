6da23400f66c1fa90dc90762b591fea8956cb7e1
jinwoong-bang <jwbang@malgn.com>
2022-02-02 17:47:14 +0900

    feature : 사용할 색상을 상수로 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bang-bg-dark': '#1D1D1D',
+        'bang-primary': '#5571FD',
+        'bang-secondary': '#C2306C',
+      },
+    },
   },
   plugins: [],
 };
