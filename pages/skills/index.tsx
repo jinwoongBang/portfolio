4384c89660d9dc56e69f995b126ff739ce7875e6
jinwoong-bang <jwbang@malgn.com>
2022-02-12 14:40:44 +0900

    refactor : 스킬 페이지 그리드로 레이아웃 구현

--- pages/skills/index.tsx
+++ pages/skills/index.tsx
@@ -2,13 +2,16 @@ import type { NextPage } from 'next';
 
 function Skills() {
   return (
-    <div className="container mx-auto">
-      <div className="grid grid-cols-2">
-        <div className="col-span-1 border border-white">
-          <h1>[skill] 왼쪽 레레이이아아웃</h1>
+    <div className="container mx-auto border">
+      <div className="grid grid-rows-2 grid-cols-4 grid-flow-col">
+        <div className="row-span-1 col-span-3 border">
+          <h1>[skill] 왼쪽 상단 레이아아웃</h1>
         </div>
-        <div className="col-span-1 border border-white">
-          <h1>[skill] 오른쪽 레레이이아아웃</h1>
+        <div className="row-span-1 col-span-3 border">
+          <h1>[skill] 왼쪽 하단 레이아아웃</h1>
+        </div>
+        <div className="row-span-2 col-span-1 border">
+          <h1>[skill] 오른쪽 레이아아웃</h1>
         </div>
       </div>
     </div>
